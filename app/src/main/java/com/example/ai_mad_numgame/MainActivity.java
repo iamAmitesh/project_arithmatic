bb1fe6004fe7e7793cf49fabecd4f2bb41fe176a
Jyotik235 <201b342@juetguna.in>
2022-05-26 18:51:26 +0530

    test

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.ai_mad_numgame;
 /*
    App will show your last performance at the start of the activity. New Tournament will start from
    all performance set to -1 again. And your new performance will be visible, when you return back to game
-jyoti kumari
+    hello myself jonti kumari
 */
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
